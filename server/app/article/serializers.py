d64136eeddb9a8b300577311a71d540747e89372
nakajimamoto340 <nakajimamoto340@gmail.com>
2024-08-02 22:08:10 +0900

    article_postの実装

--- server/app/article/serializers.py
+++ server/app/article/serializers.py
@@ -6,5 +6,5 @@ from .models import Article
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
-        fields = ["id", "title", "auther", "content", "created_at", "updated_at"]
+        fields = "__all__"
         read_only_fields = ["id", "created_at", "updated_at"]
--- server/app/article/views.py
+++ server/app/article/views.py
@@ -1,8 +1,9 @@
 # from django.contrib.auth import logout as auth_logout
-# from django.shortcuts import redirect  # render
-from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, views
+from rest_framework.response import Response
 
-# from .models import Article
+from .models import Article
 from .serializers import ArticleSerializer
 
 # def login(request):
@@ -16,3 +17,13 @@ from .serializers import ArticleSerializer
 
 class ArticleCreateView(generics.CreateAPIView):
     serializer_class = ArticleSerializer
+
+
+class ArticleDetailView(views.APIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+    def get(self, request, uuid, *args, **kwargs):
+        article = get_object_or_404(Article, id=uuid)
+        serializer = ArticleSerializer(instance=article)
+        return Response(serializer.data)
