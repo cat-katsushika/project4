91136a0239a69519e595876190c8d80e9ea6059e
nakajimamoto340 <nakajimamoto340@gmail.com>
2024-08-03 17:33:43 +0900

    article_list_getの実装

--- server/app/article/urls.py
+++ server/app/article/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ArticleCreateView
+from .views import ArticleCreateView, ArticleListView
 
 app_name = "article"
 
 urlpatterns = [
     # path("auth/", include("social_django.urls", namespace="social")),
     path("create/", ArticleCreateView.as_view(), name="create"),
+    path("list/", ArticleListView.as_view(), name="list"),
 ]
--- server/app/article/views.py
+++ server/app/article/views.py
@@ -1,8 +1,9 @@
 # from django.contrib.auth import logout as auth_logout
 # from django.shortcuts import redirect  # render
+from django_filters import rest_framework as filters
 from rest_framework import generics
 
-# from .models import Article
+from .models import Article
 from .serializers import ArticleSerializer
 
 # def login(request):
@@ -16,3 +17,10 @@ from .serializers import ArticleSerializer
 
 class ArticleCreateView(generics.CreateAPIView):
     serializer_class = ArticleSerializer
+
+
+class ArticleListView(generics.ListAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    filter_backends = [filters.DjangoFilterBackend]
+    filterset_fields = "__all__"
